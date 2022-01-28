ba88a3dc864458e4d9611169d7547b068bd6e13a
harmokuwpon <97723253+harmokuwpon@users.noreply.github.com>
2022-01-28 15:20:19 +0900

    oo

--- py.py
+++ py.py
@@ -1,5 +1,29 @@
-import numpy as np
 
-a = np.array([1, 2])
-b = np.array([3, 4])
-print(a + b)
+import time
+import openpyxl
+import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from msedge.selenium_tools import Edge, EdgeOptions
+from selenium import webdriver
+
+path = r"C:\py\msedgedriver.exe"
+       #ヘッドレス
+#options.add_argument("disable-gpu")
+driver = webdriver.Edge(executable_path='C:\py\msedgedriver.exe')#EDGEドライバー保管先　保管先によって変更すること。
+driver.get('https://iab-bp.omron.co.jp/drppe/')#DRマネージャーURL
+
+
+
+def driver_init():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    return webdriver.Chrome(options=options)
+
+
+print(driver.find_element_by_xpath('/html/body/div[1]/div/section/div/div/h2').text)
+print(driver.current_url)
+driver.quit()
