d748fc7b4e9d05433f9e972d174bba9e0821d416
harmokuwpon <97723253+harmokuwpon@users.noreply.github.com>
2022-01-28 16:01:49 +0900

    Update py.py

--- py.py
+++ py.py
@@ -1,28 +1,10 @@
-
-import time
-
-import datetime
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import chromedriver_binary
 
-from selenium import webdriver
-
-path = r"C:\py\msedgedriver.exe"
-       #ヘッドレス
-#options.add_argument("disable-gpu")
-driver = webdriver.Edge(executable_path='C:\py\msedgedriver.exe')#EDGEドライバー保管先　保管先によって変更すること。
-driver.get('https://iab-bp.omron.co.jp/drppe/')#DRマネージャーURL
-
-
-
-def driver_init():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    return webdriver.Chrome(options=options)
-
-
-print(driver.find_element_by_xpath('/html/body/div[1]/div/section/div/div/h2').text)
+options = Options()
+# ヘッドレスモード（Linux上で動かすとき必ずこのモードにしておく）
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+driver.get('https://example.com')
 print(driver.current_url)
-driver.quit()
