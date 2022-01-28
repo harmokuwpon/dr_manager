1dbcbf8a3a8543c906b10e6661b26bd95fbb0ce3
harmokuwpon <97723253+harmokuwpon@users.noreply.github.com>
2022-01-28 16:16:47 +0900

    Update py.py

--- py.py
+++ py.py
@@ -1,10 +1,19 @@
+from get_chrome_driver import GetChromeDriver
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import chromedriver_binary
 
-options = Options()
-# ヘッドレスモード（Linux上で動かすとき必ずこのモードにしておく）
-options.add_argument('--headless')
-driver = webdriver.Chrome(options=options)
-driver.get('https://example.com')
+get_driver = GetChromeDriver()
+get_driver.install()
+
+def driver_init():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    return webdriver.Chrome(options=options)
+
+driver = driver_init()
+driver.get('https://hashito.biz/')
+print(driver.find_element_by_xpath('/html/body/div[1]/div/section/div/div/h2').text)
 print(driver.current_url)
+driver.quit()
+
+
+
