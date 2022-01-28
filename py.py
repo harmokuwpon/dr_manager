c43b53aef182cc7d04f63173c449a73eb14ffde8
harmokuwpon <97723253+harmokuwpon@users.noreply.github.com>
2022-01-28 15:45:58 +0900

    Update py.py

--- py.py
+++ py.py
@@ -6,8 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-from msedge.selenium_tools import Edge, EdgeOptions
+
 from selenium import webdriver
 
 path = r"C:\py\msedgedriver.exe"
